561a3bc6bbce900d606226262196ff4041ab35c4
Gregorio Nuevo Castro <gncdvp@gmail.com>
2024-01-03 12:42:34 +0100

    First working version. Needs cleaning

--- load_phi_model.py
+++ load_phi_model.py
@@ -4,10 +4,10 @@ from transformers import (
     AutoTokenizer, 
     StoppingCriteria, 
     StoppingCriteriaList, 
-    TextIteratorStreamer
     )
 from langchain.llms.huggingface_pipeline import HuggingFacePipeline
 from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
+from typing import List
 
 
 class StopOnTokens(StoppingCriteria):
